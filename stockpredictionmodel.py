75b3c7ee47599201fcac839bd92ed0d42f7b71ad
EugeneZnm <eugenenzioki@gmail.com>
2019-05-07 23:16:31 +0300

    creation of gold training and test vawiable and data

---
+++ stockpredictionmodel.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import matplotlib as plt
+
+GOLD_TRAIN_DATA = 'CSV Files/Gold Futures Historical 1 Year.csv'
+GOLD_TRAIN_DATA = 'CSV Files/Gold Futures Historical Data.csv'
